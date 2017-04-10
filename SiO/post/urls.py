9518f34f3f332302b18fafa483559f5623988b06
Andreas Jacobsen <andreas1300@gmail.com>
2017-04-10 17:46:54 +0200

    post sitt view fungerer nå.

--- SiO/post/urls.py
+++ SiO/post/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
 from . import views
+from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth import get_user_model
+from SiO.CoAdmin.models import Administrator
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+User = get_user_model()
 
 urlpatterns = [
-    url(r'^post/$', views.post, name="post")
+    url(r'^post/$', views.post.as_view(), name="post")
 ]
--- SiO/post/views.py
+++ SiO/post/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import View
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 User = get_user_model()
 
