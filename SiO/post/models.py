f2c5fd1a5aee43fbd254b529ef5b178d992bb2ad
Andreas Jacobsen <andreas1300@gmail.com>
2017-04-13 05:22:24 +0200

    Lagt til model for databasen

--- SiO/post/models.py
+++ SiO/post/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from SiO.member.models import Association
+from SiO.member.models import Member
 # Create your models here.
+from SiO.member.models import Association
+1import datetime
+import re
+
+class Meta:
+    db_table = 'email'
+
+
+class Email(models.Model):
+    sender = models.CharField(max_length=254)
+    sentTime = models.DateTimeField(auto_now_add=True, blank=False)
+
+    receiver = models.CharField(max_length=254)
+    sendStatus = models.BooleanField()
+    message = models.TextField()
+    asocNumber = models.ForeignKey(Association)
