2e9876ded57141ed14f374305ca53c1279a9d813
Andreas Jacobsen <andreas1300@gmail.com>
2017-04-13 17:15:38 +0200

    Endringer i model så ting kjører nå

--- SiO/post/models.py
+++ SiO/post/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from SiO.member.models import Association
 from SiO.member.models import Member
 # Create your models here.
-from SiO.member.models import Association
 
 
 class Meta:
     db_table = 'email'
 
-
+# TODO: gjør dette om til to klasser hvor du inndeler5 sender og tekst i to  forskjellige for å forhindre repitisjon.
 class Email(models.Model):
     sender = models.CharField(max_length=254)
     sentTime = models.DateTimeField(auto_now_add=True, blank=False)
@@ -16,4 +15,4 @@ class Email(models.Model):
     receiver = models.CharField(max_length=254)
     sendStatus = models.BooleanField()
     message = models.TextField()
-    asocNumber = models.ForeignKey(Association)
+    asocNumber = models.OneToOneField(Association)
